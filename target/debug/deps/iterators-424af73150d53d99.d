dce738de7dc62b6ac8f1c8172ff75a075977640c
psatomas <psatomas@gmail.com>
2025-10-27 17:09:48 -0300

    first commit

---
+++ target/debug/deps/iterators-424af73150d53d99.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/iterators/target/debug/deps/iterators-424af73150d53d99.d: src/main.rs
+
+/home/tomas/Documents/projects/rust/iterators/target/debug/deps/libiterators-424af73150d53d99.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/iterators-46697a199e048031.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/iterators/target/debug/deps/iterators-46697a199e048031.d: src/main.rs
+
+/home/tomas/Documents/projects/rust/iterators/target/debug/deps/libiterators-46697a199e048031.rmeta: src/main.rs
+
+src/main.rs:
