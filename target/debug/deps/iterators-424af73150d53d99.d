f4e6d5dbbdc7f7de92bb4ab5d5e3bc4431707c1a
psatomas <psatomas@gmail.com>
2025-10-28 18:30:07 -0300

    feat: add manual iteration example

--- target/debug/deps/iterators-424af73150d53d99.d
+++
@@ -1,5 +0,0 @@
-/home/tomas/Documents/projects/rust/iterators/target/debug/deps/iterators-424af73150d53d99.d: src/main.rs
-
-/home/tomas/Documents/projects/rust/iterators/target/debug/deps/libiterators-424af73150d53d99.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/iterators-46697a199e048031.d
+++
@@ -1,5 +0,0 @@
-/home/tomas/Documents/projects/rust/iterators/target/debug/deps/iterators-46697a199e048031.d: src/main.rs
-
-/home/tomas/Documents/projects/rust/iterators/target/debug/deps/libiterators-46697a199e048031.rmeta: src/main.rs
-
-src/main.rs:
